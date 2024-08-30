e5228ba5561e7d9cbfda0347116903973428bfd3
MrTango <md@derico.de>
2024-08-30 12:21:17 +0300

    Back to development: 0.1a3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.checklist",
-    version="0.1a2",
+    version="0.1a3.dev0",
     description="Checklist App for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
